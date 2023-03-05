effd483a0a2cbbe7507df0d635b7ace9f7dd8dbd
u84556zt <zhong.tang-3@student.manchester.ac.uk>
2023-03-05 14:56:41 +0000

    Created enum class for card values

---
+++ Rank.java
@@ -0,0 +1,19 @@
+/** Contain the rank of the card that will form the deck. 
+ * Enum cannot hold integer element. They can only hold 
+ * strings as element */
+
+enum Rank {
+	Ace,
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King
+}
