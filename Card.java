5b8b779e458b0890ad3397c9e6eaf0a23d25780b
u84556zt <zhong.tang-3@student.manchester.ac.uk>
2023-03-05 15:39:51 +0000

    Added constructor to create card objects

--- Card.java
+++ Card.java
@@ -1,9 +1,26 @@
-/** A card needs to have a Rank and a Suit hence that will be its constructor
- * */
+/** A card needs to have a Rank and a Suit hence that will be its constructor.
+ *  The toString() method is automatically called everytime we call this object 
+ *  in a println().
+ */
 
 public class Card {
-	private String 
+	private Suit suit; 
+	private Rank rank;
+
 	public Card(Suit suit, Rank rank) {
-		
-	}    
+		this.suit = suit;
+		this.rank = rank;
+	}
+
+	public String toString() {
+		return (rank + " of " + suit);
+	}
+
+	public int getValue() {
+		return rank.getValue();
+	}
+
+	public String getSuit() {
+		return suit.getSuit();
+	}
 }
--- MainGame.java
+++ MainGame.java
@@ -5,5 +5,9 @@ public class MainGame{
 
 	public static void main(String[] args){
 		System.out.println("Welcome to my Blackjack game");
+		Suit a = Suit.CLUBS;
+		Rank b = Rank.ACE;
+		Card c = new Card(a, b);
+		System.out.println(c.getSuit());
 	}
 }
